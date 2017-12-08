a90e5b812590a067207fffce7e24531e7680baa6
jhangshuoluoluo <31992517+jhangshuoluoluo@users.noreply.github.com>
2017-12-08 23:50:44 +0800

    Add files via upload

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -7,7 +7,7 @@ else
     # file not exist
     echo "File does not exists."
     wget --no-check-certificate "https://www.dropbox.com/s/hgqff868k6e4x5x/weights03-0.81.hdf5?dl=1" -O weights03-0.81.hdf5
-
+    wget --no-check-certificate "https://www.dropbox.com/s/73ytmdy1wng8ffi/w2v_size128.model.bin?dl=1" -O w2v_size128.model.bin
 fi
 
 
